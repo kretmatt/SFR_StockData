a2451ad61ad14b046131624c2ace09cb33fb55a2
kretmatt <mrtthias@gmail.com>
2023-04-21 15:54:14 +0200

    Add filter on stock page to find entries with price greater than the entered value

--- stockdata_ui/pages/stock/[bondName].tsx
+++ stockdata_ui/pages/stock/[bondName].tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
 const inter = Inter({ subsets: ['latin'] })
+import { useState } from 'react'
 
 
 export async function getServerSideProps(context) {
@@ -17,15 +18,33 @@ export async function getServerSideProps(context) {
 }
 
 export default function Home({ data}) {
+const [query, setQuery] = useState('');
+
+  const searchFilter = (array) => {
+	  return array.filter(
+	    (el) => el.price>(query)
+	  )
+  }
+
+  const filtered = searchFilter(data)
+
+const handleChange = (e) => {
+setQuery(e.target.value);
+}
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       <h1><Link href="/">{data[0]?.bondName}</Link></h1>
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-1 lg:text-left">
+       <h1><Link href="/">            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              &lt;-
+            </span> {data[0]?.bondName}</Link></h1>
+            <input onChange={(e)=>handleChange(e)} type='text' placeholder='Search...'/>
+      <div className="mb-128 grid text-center lg:mb-12 lg:grid-cols-1 lg:text-left">
       
        {data?.Length === 0 ? (
         <h1>test</h1>
        ):(
-         data.map((data) => (
+         filtered.map((data) => (
 			<div
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
